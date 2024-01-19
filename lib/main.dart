3671fd2d6523a7ba2188cce737fdbe94f0394bb1
lucas-daniel202 <lucas.dan.melo@gmail.com>
2024-01-19 12:56:01 -0300

    página de resultados

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
@@ -204,8 +203,24 @@ class SecondPage extends StatelessWidget {
   }
 }
 
-class QuestionarioPage extends StatelessWidget {
+class QuestionarioPage extends StatefulWidget {
   const QuestionarioPage({Key? key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _QuestionarioPageState createState() => _QuestionarioPageState();
+}
+
+class _QuestionarioPageState extends State<QuestionarioPage> {
+  int? idade;
+  String? genero;
+  double? altura;
+  double? peso;
+  String? colesterol;
+  String? glicose;
+  bool? consomeAlcool;
+  bool? fuma;
+  bool? praticaExercicio;
 
   @override
   Widget build(BuildContext context) {
@@ -227,28 +242,86 @@ class QuestionarioPage extends StatelessWidget {
               decoration: const InputDecoration(
                 labelText: 'Diga a sua idade:'
               ),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  idade = int.tryParse(value);
+                });
+              },
             ),
-            _buildDropdownFormField('Qual o seu gênero?', ['Feminino', 'Masculino']),
+            _buildDropdownFormField('Qual o seu gênero?', ['Masculino', 'Feminino'], (String? value) {
+              setState(() {
+                genero = value;
+              });
+            }),
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Diga sua altura (m):',
                 helperText: 'Ex: 1.70',
               ),
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
+                setState(() {
+                  altura = double.tryParse(value);
+                });
+              },
             ),
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Diga o seu peso (kg):',
                 helperText: 'Ex: 80.0',
               ),
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
+                setState(() {
+                  peso = double.tryParse(value);
+                });
+              },
             ),
-            _buildDropdownFormField('Como está a sua taxa de colesterol?', ['Normal', 'Acima do Normal', 'Muito Acima do Normal']),
-            _buildDropdownFormField('Como está a taxa da sua glicose?', ['Normal', 'Acima do Normal', 'Muito Acima do Normal']),
-            _buildDropdownFormField('Você consome álcool?', ['Não', 'Sim']),
-            _buildDropdownFormField('Você tem o costume de fumar?', ['Não', 'Sim']),
-            _buildDropdownFormField('Pratica algum exercício físico?', ['Não', 'Sim']),
+            _buildDropdownFormField('Como está a sua taxa de colesterol?', ['Normal', 'Acima do Normal', 'Muito Acima do Normal'], (String? value) {
+              setState(() {
+                colesterol = value;
+              });
+            }),
+            _buildDropdownFormField('Como está a taxa da sua glicose?', ['Normal', 'Acima do Normal', 'Muito Acima do Normal'], (String? value) {
+              setState(() {
+                glicose = value;
+              });
+            }),
+            _buildDropdownFormField('Você consome álcool?', ['Não', 'Sim'], (String? value) {
+              setState(() {
+                consomeAlcool = value == 'Sim';
+              });
+            }),
+            _buildDropdownFormField('Você tem o costume de fumar?', ['Não', 'Sim'], (String? value) {
+              setState(() {
+                fuma = value == 'Sim';
+              });
+            }),
+            _buildDropdownFormField('Pratica algum exercício físico?', ['Não', 'Sim'], (String? value) {
+              setState(() {
+                praticaExercicio = value == 'Sim';
+              });
+            }),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResultadoPage(
+                      idade: idade ?? 0,
+                      genero: genero ?? '',
+                      altura: altura ?? 0.0,
+                      peso: peso ?? 0.0,
+                      colesterol: colesterol ?? '',
+                      glicose: glicose ?? '',
+                      consomeAlcool: consomeAlcool ?? false,
+                      fuma: fuma ?? false,
+                      praticaExercicio: praticaExercicio ?? false,
+                    ),
+                  ),
+                );
               },
               child: const Text('Enviar Respostas'),
             ),
@@ -258,7 +331,7 @@ class QuestionarioPage extends StatelessWidget {
     );
   }
 
-  Widget _buildDropdownFormField(String labelText, List<String> options) {
+  Widget _buildDropdownFormField(String labelText, List<String> options, void Function(String?) onChanged) {
     return DropdownButtonFormField<String>(
       decoration: InputDecoration(labelText: labelText),
       items: options.map((String option) {
@@ -267,8 +340,132 @@ class QuestionarioPage extends StatelessWidget {
           child: Text(option),
         );
       }).toList(),
-      onChanged: (String? value) {
-      },
+      onChanged: onChanged,
+    );
+  }
+}
+
+class ResultadoPage extends StatelessWidget {
+  final int idade;
+  final String genero;
+  final double altura;
+  final double peso;
+  final String colesterol;
+  final String glicose;
+  final bool consomeAlcool;
+  final bool fuma;
+  final bool praticaExercicio;
+
+  const ResultadoPage({
+    Key? key,
+    required this.idade,
+    required this.genero,
+    required this.altura,
+    required this.peso,
+    required this.colesterol,
+    required this.glicose,
+    required this.consomeAlcool,
+    required this.fuma,
+    required this.praticaExercicio,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    bool temDoencaCardiovascular = false;
+    double probabilidade = 0.0;
+
+    
+    if (idade > 50) {
+      probabilidade += 0.2;
+    }
+
+    if (genero == 'Feminino') {
+      probabilidade += 0.1;
+    }
+
+    double imc = peso / (altura * altura);
+    if (imc >= 25) {
+      probabilidade += 0.15;
+    }
+
+    if (colesterol == 'Normal') {
+      probabilidade += 0.05;
+    } else if (colesterol == 'Acima do Normal') {
+      probabilidade += 0.15;
+    } else if (colesterol == 'Muito Acima do Normal') {
+      probabilidade += 0.3;
+    }
+
+    if (glicose == 'Normal') {
+      probabilidade += 0.05;
+    } else if (glicose == 'Acima do Normal') {
+      probabilidade += 0.15;
+    } else if (glicose == 'Muito Acima do Normal') {
+      probabilidade += 0.3;
+    }
+
+    if (consomeAlcool) {
+      probabilidade += 0.15;
+    }
+
+    if (fuma) {
+      probabilidade += 0.3;
+    }
+
+    if (praticaExercicio) {
+      probabilidade -= 0.2;
+    }
+
+  
+    if (probabilidade >= 0.7) {
+      temDoencaCardiovascular = true;
+    }
+
+    String resultado = temDoencaCardiovascular
+        ? 'Alta probabilidade de doença cardiovascular'
+        : 'Baixa probabilidade de doença cardiovascular';
+
+    int porcentagem = (probabilidade * 100).round();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Resultados',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              resultado,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Probabilidade: $porcentagem%',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Voltar ao Questionário'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
